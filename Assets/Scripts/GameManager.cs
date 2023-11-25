d3371c45d065353f4277d4c3ca1bdb25caab6cbc
Onnils <46597283+Onnils@users.noreply.github.com>
2023-11-25 16:17:18 +0100

    Start Screen
    With help of the AMAZING potato.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -52,7 +53,12 @@ public class GameManager : MonoBehaviour
 
         OnStateChanged?.Invoke(state);
     }
-    
+    public void ChanceScene(int SceneIndex) 
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+
 }
 
 public enum GameState
