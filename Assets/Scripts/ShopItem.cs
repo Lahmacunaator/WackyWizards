93e91fbe6704de9cafc0929324e80755244cbbd9
Barış Can Soy <43369191+Lahmacunaator@users.noreply.github.com>
2023-11-25 19:25:36 +0200

    shop related stuff

---
+++ Assets/Scripts/ShopItem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShopItem : MonoBehaviour
+{
+    [HideInInspector]
+    public ModifierSO modifier;
+    [SerializeField] private TMP_Text buttonText;
+
+    public void UpdateModifier(ModifierSO modifierData)
+    {
+        modifier = modifierData;
+        buttonText.text = modifierData.name;
+    }
+    
+    public void SelectModifier()
+    {
+        GameManager.Instance.AddModifier(modifier);
+    }
+}
